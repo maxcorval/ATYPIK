0e01da0dad664c7f100dd2d2e128e0c32aa997e0
maxcorval <vidjinelodji@yahoo.com>
2022-04-25 22:13:48 +0200

    first commit

---
+++ app/Http/Controllers/AdresseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\adresse;
+use Illuminate\Http\Request;
+
+class AdresseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\adresse  $adresse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(adresse $adresse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\adresse  $adresse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(adresse $adresse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\adresse  $adresse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, adresse $adresse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\adresse  $adresse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(adresse $adresse)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/EquipementController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\equipement;
+use Illuminate\Http\Request;
+
+class EquipementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\equipement  $equipement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(equipement $equipement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\equipement  $equipement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(equipement $equipement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\equipement  $equipement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, equipement $equipement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\equipement  $equipement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(equipement $equipement)
+    {
+        //
+    }
+}
